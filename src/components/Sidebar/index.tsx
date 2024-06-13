13b828871e7c80a6f53b6077d8004971cea9b148
KrizRoMe <cmromero1231@gmail.com>
2024-06-13 11:48:02 -0500

    fix: update course navbar link

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -378,7 +378,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                 <Link
                   href="/dashboard/risk-course"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("risk-course") &&
+                    pathname.includes("dashboard/risk-course") &&
                     "bg-graydark dark:bg-meta-4"
                   }`}
                 >
@@ -414,9 +414,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               {/* <!-- Menu Item Cursos --> */}
               <li>
                 <Link
-                  href="/dashboard/cursos"
+                  href="/dashboard/course"
                   className={`group relative flex items-center gap-2.5 rounded-sm px-4 py-2 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                    pathname.includes("cursos") && "bg-graydark dark:bg-meta-4"
+                    pathname.includes("dashboard/course") &&
+                    "bg-graydark dark:bg-meta-4"
                   }`}
                 >
                   <svg
