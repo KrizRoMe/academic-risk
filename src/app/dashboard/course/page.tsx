3287346a6471148b2fd1af76f695f76e67c16b53
KrizRoMe <cmromero1231@gmail.com>
2024-07-05 08:12:17 -0500

    feat: save year in localstorage

--- src/app/dashboard/course/page.tsx
+++ src/app/dashboard/course/page.tsx
@@ -2,44 +2,37 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import TableCustom from "@/components/Tables/TableCustom";
+import { getCourses } from "@/services/course.service";
 
 export const metadata: Metadata = {
   title: "AcademicRisk | Cursos",
 };
 
 const columns = [
-  { header: "Curso", field: "curso" },
-  { header: "Docente", field: "docente" },
-];
-const data = [
-  {
-    curso: "Calculo Diferencial",
-    docente: "Roberto Perales",
-  },
-  {
-    curso: "Calculo integral",
-    docente: "Roberto Perales",
-  },
-  {
-    curso: "Fisica",
-    docente: "Victor Cabrera Abanto",
-  },
-  {
-    curso: "Fisica Moderna",
-    docente: "Victor Cabrera Abanto",
-  },
+  { header: "Curso", field: "name" },
+  { header: "Código", field: "code" },
 ];
 
-const CoursePage = () => {
+interface Course {
+  name: string;
+  code: string;
+}
+
+interface CoursePageProps {
+  courses: Course[];
+}
+
+export default async function CoursePage() {
+
+  const courses: Course[] = await getCourses();
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Cursos" />
 
       <div className="flex flex-col gap-10">
-        <TableCustom columns={columns} data={data}></TableCustom>
+        <TableCustom columns={columns} data={courses}></TableCustom>
       </div>
     </DefaultLayout>
   );
-};
-
-export default CoursePage;
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,16 @@ import Link from "next/link";
 import DarkModeSwitcher from "./DarkModeSwitcher";
 import DropdownUser from "./DropdownUser";
 import Image from "next/image";
+import { useState } from "react";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
   setSidebarOpen: (arg0: boolean) => void;
   isShowUser: boolean;
 }) => {
+
+  const [selectedYear, setSelectedYear] = useState(localStorage.getItem('selectedYear') || '');
+
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between gap-2 px-4 py-4 shadow-2 md:px-6 2xl:px-12">
@@ -98,7 +102,14 @@ const Header = (props: {
                 priority
               />
             </span>
-            <select className="relative z-20 w-full appearance-none rounded border bg-transparent px-12 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ">
+            <select 
+            className="relative z-20 w-full appearance-none rounded border bg-transparent px-12 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input "
+            value={selectedYear}
+            onChange={(e) => {
+              localStorage.setItem('selectedYear', e.target.value);
+              setSelectedYear(e.target.value);
+            }}
+            >
               <option value="" className="text-body dark:text-bodydark">
                 Seleccionar Año
               </option>
