f96bdb2e82905313c804e2e4bbc2eaa7350fecea
KrizRoMe <cmromero1231@gmail.com>
2024-07-12 03:27:31 -0500

    feat: add loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,39 @@
-import type { Metadata } from "next";
+"use client";
+
+// import type { Metadata } from "next";
 import "@/css/satoshi.css";
 import "@/css/style.css";
 import Providers from "@/components/Providers";
+import { useEffect, useState } from "react";
+import Loader from "@/components/common/Loader";
 
-export const metadata: Metadata = {
-  title: "AcademicRisk",
-  description:
-    "Web Application focused on reduce the rate of students at academic risk.",
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
+// export const metadata: Metadata = {
+//   title: "AcademicRisk",
+//   description:
+//     "Web Application focused on reduce the rate of students at academic risk.",
+//   icons: {
+//     icon: "/favicon.ico",
+//   },
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 1000);
+  }, []);
+
   return (
     <html lang="en">
       <body>
         <Providers>
-          <div className="dark:bg-boxdark-2 dark:text-bodydark">{children}</div>
+          <div className="dark:bg-boxdark-2 dark:text-bodydark">
+            {isLoading ? <Loader /> : children}
+          </div>
         </Providers>
       </body>
     </html>
