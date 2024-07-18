e5050281ab7cf7fddfc696b4df9b1e7b798f1f68
KrizRoMe <cmromero1231@gmail.com>
2024-07-17 21:10:15 -0500

    chore: add some config changes

--- src/app/api/whatsapp/route.ts
+++ src/app/api/whatsapp/route.ts
@@ -7,31 +7,33 @@ export async function POST(request: Request) {
   try {
     const { phone } = await request.json();
     const atRiskCourses = await getRiskCourses();
+    const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+    const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
 
     const riskCoursesFormatted = atRiskCourses.map((risCourse) => {
       return `* ${risCourse.course}`;
     });
-
     const message = `Espero que se encuentre bien. Me gustaría informarle sobre el desempeño académico de su hijo/a . Actualmente, el estudiante Cristopher Romero Medrano, esta en riesgo académico en los siguientes cursos: \n\n ${riskCoursesFormatted.join(
       "\n",
     )} \n\n Por favor, comuníquese con el tutor académico de su hijo/a para obtener más información y apoyo. Gracias.`;
 
     // Logic to send message using Twilio
-    const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-    const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
-    const client = twilio(twilioAccountSid, twilioAuthToken);
-
-    const twilioMessage = await client.messages.create({
-      body: message,
-      from: process.env.TWILIO_PHONE_NUMBER,
-      to: phone,
-    });
+    try {
+      const client = twilio(twilioAccountSid, twilioAuthToken);
+      const twilioMessage = await client.messages.create({
+        body: message,
+        from: process.env.TWILIO_PHONE_NUMBER,
+        to: phone,
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
     // Logic to send message using WhatsappClient
     // const formattedPhone = `${phone}@c.us`;
     // await WhatsappClient.sendMessage(formattedPhone, message);
 
-    return NextResponse.json({ twilioMessage });
+    return NextResponse.json({ message: "Message sent" });
   } catch (error) {
     return NextResponse.json({ error });
   }
