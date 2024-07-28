71d411034ff468b169f04faf5cf80de076875093
joel <joeljhojan9@gmail.com>
2024-07-28 05:31:10 -0500

    feat: obtener informacion dinamica del estudiante

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -28,3 +28,44 @@ export async function getCourses() {
     throw new Error(`Database error!`);
   }
 }
+
+
+export async function getCoursesByStudentId(studentId: number) {
+  try {
+    const grades = await prisma.grade.findMany({
+      where: {
+        studentId: studentId,
+      },
+      select: {
+        course: {
+          select: {
+            id: true,
+            name: true,
+            code: true,
+            teacherId: true,
+            semester: true,
+            year: true,
+            teacher: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const courses = grades.map((grade) => grade.course);
+    const uniqueCourses = Array.from(new Set(courses.map((course) => course.id))).map((id) => 
+      courses.find((course) => course.id === id)
+    );
+    const flattenedCourses = uniqueCourses.map((course) => ({
+      ...course,
+      teacher: course?.teacher.name,
+    }));
+
+    return flattenedCourses;
+  } catch (error) {
+    throw new Error(`Database error!`);
+  }
+}
--- src/app/services/riskcourse.service.ts
+++ src/app/services/riskcourse.service.ts
@@ -39,3 +39,55 @@ export async function getRiskCourses() {
     throw new Error(`Database error! : ${error}`);
   }
 }
+
+export async function getRiskGradesByStudentId(studentId: number) {
+  try {
+    const grades = await prisma.grade.findMany({
+      where: {
+        studentId: studentId,
+      },
+      select: {
+        courseId: true,
+        academicPeriodId: true,
+        value1: true,
+        value2: true,
+        value3: true,
+        updatedAt: true,
+        createdAt: true,
+        course: {
+          select: {
+            name: true,
+            code: true,
+          },
+        },
+        academicPeriod: {
+          select: {
+            semester: true,
+            year: {
+              select: {
+                year: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const flattenedGrades = grades.map((grade) => {
+      const average = (grade.value1 + grade.value2 + grade.value3) / 3;
+      const failures = average < 10.5 ? 1 : 0;
+
+    return {
+        ...grade,
+        failures: failures,
+        semester: grade.academicPeriod.semester,
+        course: grade.course.name,
+        courseCode: grade.course.code,
+      };
+    });
+
+    return flattenedGrades;
+  } catch (error) {
+    throw new Error(`Database error! : ${error}`);
+  }
+}
